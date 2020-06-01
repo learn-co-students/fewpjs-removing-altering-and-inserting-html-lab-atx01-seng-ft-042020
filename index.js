9af5dd68631f8782862b89e08677d2514304f37b
marshall405 <marshall.slemp@gmail.com>
2020-06-01 14:46:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Marshall is the champion';
